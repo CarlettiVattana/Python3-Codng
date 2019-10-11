42b89e153c481b3b512d4c3abfff0d421e2db03b
PattanaWattana <patcom.frost@gmail.com>
2019-10-11 16:27:48 +0700

    Update and rename Day I to Day1.py

---
+++ Day1.py
@@ -0,0 +1,14 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+ii = int(input())
+dd = float(input())
+ss = input()
+
+print (i + ii)
+
+print (d + dd)
+
+print (s + ss)
+# The 's' variable above should be printed first.
